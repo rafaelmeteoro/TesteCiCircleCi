acbc150ede4d9e9b0eee15e162c48c87d4eff40a
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2019-02-05 20:16:17 -0200

    force again only in master

--- app/src/main/java/com/meteoro/testecicircleci/validator/EmailValidator.kt
+++ app/src/main/java/com/meteoro/testecicircleci/validator/EmailValidator.kt
@@ -3,6 +3,6 @@ package com.meteoro.testecicircleci.validator
 class EmailValidator : Validator {
 
     override fun validate(input: String): Boolean {
-        return false
+        return true
     }
 }
