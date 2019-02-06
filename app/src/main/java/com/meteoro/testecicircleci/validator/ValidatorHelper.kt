dfbb4d406fa658a8f0646e4e55ba55856e97a8ba
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2019-02-06 17:26:57 -0200

    validate helper

--- app/src/main/java/com/meteoro/testecicircleci/validator/ValidatorHelper.kt
+++ app/src/main/java/com/meteoro/testecicircleci/validator/ValidatorHelper.kt
@@ -2,7 +2,14 @@ package com.meteoro.testecicircleci.validator
 
 class ValidatorHelper {
 
-    fun calculateDigit(str: String): Int {
-        return 0
+    fun calculateDigit(str: String, peso: IntArray): Int {
+        var soma = 0
+        val size = str.length - 1
+        for (indice in size downTo 0) {
+            val digito = str.substring(indice, indice + 1).toInt()
+            soma += digito * peso[peso.size - str.length + indice]
+        }
+        soma = 11 - soma % 11
+        return if (soma > 9) 0 else soma
     }
 }
